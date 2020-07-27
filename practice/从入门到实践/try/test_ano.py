688def8f09850d0dccfcd5689d08e28e03b3c462
AmberJar <fpcfpc1995>
2020-07-27 20:36:42 +0100

    test

---
+++ practice/从入门到实践/try/test_ano.py
@@ -0,0 +1,16 @@
+import unittest
+from practice.从入门到实践.language import AnonymousSurvey
+
+class TestAnonmyousSurvey(unittest.TestCase):
+
+    def test_score_single_response(self):
+        question = "What language did you first learn to speak?"
+
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+        for response in responses:
+             self.assertIn(response, my_survey.responses)
+
+unittest.main()
