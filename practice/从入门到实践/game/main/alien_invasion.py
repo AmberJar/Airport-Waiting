643a6fceefc64aafed8c44811cda5eaa06f62f54
AmberJar <fpcfpc1995>
2020-07-29 10:17:20 +0100

    alien game

--- practice/从入门到实践/game/main/alien_invasion.py
+++ practice/从入门到实践/game/main/alien_invasion.py
@@ -1,36 +1,32 @@
-import sys
 import pygame
+
 from practice.从入门到实践.game.settings import Settings
-from practice.从入门到实践.game.settings import Ship
+from practice.从入门到实践.game.ship import Ship
+import practice.从入门到实践.game.game_functions as gf
+
+from pygame.sprite import Group
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screnn_height)
+        (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
-    ship = Ship(screen)
-
-    #设置背景颜色
-
-    bg_color = (230, 230, 230)
+    ship = Ship(ai_settings, screen)
+    #创建一个用于储存的编组
+    bullets = Group()
 
     #开始游戏的主循环
     while True:
-
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        #每次循环的时候都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
