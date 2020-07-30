b01d3d768b544ec66abf5a19b722b63b0f9c8d73
AmberJar <fpcfpc1995>
2020-07-30 19:06:07 +0100

    game finished

--- practice/从入门到实践/game/main/alien_invasion.py
+++ practice/从入门到实践/game/main/alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 from practice.从入门到实践.game.game_stats import GameStats
 from practice.从入门到实践.game.settings import Settings
 from practice.从入门到实践.game.ship import Ship
+from practice.从入门到实践.game.button import Button
+from practice.从入门到实践.game.scroeboard import Scoreboard
 import practice.从入门到实践.game.game_functions as gf
 
 from pygame.sprite import Group
@@ -14,7 +16,13 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
+
+    #创建PLAY按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    #创建储存游戏统计信息的实例，并创建记分牌
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     #创建一艘飞船
     ship = Ship(ai_settings, screen)
@@ -28,13 +36,13 @@ def run_game():
     #开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
